bd09cfa7a7661142d6e3862b699eada509c21b54
Michael Morfessis <Michael.Morfessis@globalinformationsystems.com>
2017-10-06 14:05:37 -0500

    Updated

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+
+$(document).ready(function() {
+
+//each character in the game has 3 attributes health points, attack power, and counter attack power
+
+var characters = {
+
+	"Obi-Wan Kenobi": {
+		Name: "Obi-Wan Kenobi"
+		Health: 120
+		Attack: 10
+		Counter: 5
+		ImageURL : "assets/images/obiwan.jpg"
+	},
+
+	"Luke Skywalker": {
+		Name: "Luke Skywalker"
+		Health: 100
+		Attack: 6
+		Counter: 12
+		ImageURL : "assets/images/lukeskywalker.jpg"
+	},
+
+	"Darth Vader": {
+		Name: "Darth Vader"
+		Health: 150
+		Attack: 8
+		Counter: 6
+		ImageURL : "assets/images/darthvader.jpg"
+	},
+
+	"Darth Sidious": {
+		Name: "Darth Sidious"
+		Health: 180
+		Attack: 4
+		Counter: 15
+		ImageURL : "assets/images/darthvader.jpg"
+	}
+};
+
+$("button")
+
+
+}
