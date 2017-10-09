5b2dc5755f516625da7f2a640c8f90cfb2f6e3ab
Michael Morfessis <Michael.Morfessis@globalinformationsystems.com>
2017-10-09 07:17:33 -0500

    Crystals Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,69 @@
+var targetNumber = Math.floor(Math.random() * 150) + 21;
 
-$(document).ready(function() {
+$("#randomNumber").html(targetNumber);
 
-//each character in the game has 3 attributes health points, attack power, and counter attack power
+var counter = 0;
 
-var characters = {
+var wins = 0;
 
-	"Obi-Wan Kenobi": {
-		Name: "Obi-Wan Kenobi"
-		Health: 120
-		Attack: 10
-		Counter: 5
-		ImageURL : "assets/images/obiwan.jpg"
-	},
+var losses = 0;
 
-	"Luke Skywalker": {
-		Name: "Luke Skywalker"
-		Health: 100
-		Attack: 6
-		Counter: 12
-		ImageURL : "assets/images/lukeskywalker.jpg"
-	},
+var numberOptions = [10, 5, 3, 7];
 
-	"Darth Vader": {
-		Name: "Darth Vader"
-		Health: 150
-		Attack: 8
-		Counter: 6
-		ImageURL : "assets/images/darthvader.jpg"
-	},
 
-	"Darth Sidious": {
-		Name: "Darth Sidious"
-		Health: 180
-		Attack: 4
-		Counter: 15
-		ImageURL : "assets/images/darthvader.jpg"
-	}
+
+for (var i = 0; i < numberOptions.length; i++) {
+
+	var imageCrystal = $("<img class='crystalMargin' width='50' height='50'>");
+
+	imageCrystal.addClass("crystal-image");
+
+	imageCrystal.attr("src", "assets/images/crystal1.jpg");
+
+	imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+
+	$("#crystals").append(imageCrystal)
+
+
 };
 
-$("button")
+function resetGame() {
 
+	counter = 0;
+	targetNumber = Math.floor(Math.random() * 150) + 21;
+
+	$("#randomNumber").html(targetNumber);
+	$("#yourScore").html(counter)
+
+	
+};
+
+$(".crystal-image").on("click", function() {
+
+var crystalValue = ($(this).attr("data-crystalvalue"));
+crystalValue = parseInt(crystalValue);
+
+counter += crystalValue;
+
+if (counter === targetNumber) {
+	alert("You win!");
+	resetGame();
+	wins++
+	var updateGame = "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>";
+	$("#updateGame").html(updateGame);
 
 }
+
+else if (counter >= targetNumber) {
+	alert("You Lose!")
+	resetGame();
+	losses++
+	var updateGame = "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>";
+	$("#updateGame").html(updateGame);
+}
+
+else {
+	$("#yourScore").html(counter);
+}
+
+});
